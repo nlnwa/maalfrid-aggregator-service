e8eec29b92d2cab5d879b8fa06e86b01eda5407b
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2019-05-13 16:58:16 +0200

    Cursor in rethinkdb-ts is asyncIterator if node version >= 10

--- test/concurrent.spec.js
+++ test/concurrent.spec.js
@@ -1,63 +1,24 @@
 const { describe, it } = require('mocha')
 const { assert } = require('chai')
+const { Readable } = require('stream')
+const { rowCollector, map } = require('../lib/concurrent')
 
-const { makeAsyncIterator, rowCollector, map } = require('../lib/concurrent')
-
-function isAsyncIterator (iterator) {
-  return iterator &&
-    typeof iterator[Symbol.asyncIterator] === 'function' &&
-    typeof iterator[Symbol.asyncIterator]().next === 'function'
-}
-
-const getCursorMock = ({ shouldThrow = false, cursorSize = 10 } = {}) => ({
-  count: 0,
-  next: function () {
-    if (this.count >= cursorSize) {
-      if (shouldThrow) {
-        throw Error('fail')
+const getCursorMock = ({ cursorSize = 10 } = {}) => {
+  let count = cursorSize
+  return new Readable({
+    objectMode: true,
+    read (size) {
+      if (count > 0) {
+        count--
+        return this.push({ count })
       } else {
-        const err = Error('No more rows in the cursor.')
-        err.name = 'ReqlDriverError'
-        throw err
+        return this.push(null)
       }
     }
-    this.count++
-    return Promise.resolve({ count: this.count })
-  }
-})
+  })
+}
 
 describe('lib/concurrent.js', () => {
-  describe('makeAsyncIterator (of RethinkDB cursor)', async () => {
-    it('returns an async iterator', () => {
-      assert.isTrue(isAsyncIterator(makeAsyncIterator(getCursorMock())))
-    })
-
-    it('the returned async iterator iterates until exhaustion', async () => {
-      const cursorSize = 20
-      const asyncIterator = makeAsyncIterator(getCursorMock({ cursorSize }))
-      let count = 0
-      for await (const value of asyncIterator) {
-        assert.isOk(value)
-        count++
-      }
-      assert.equal(count, cursorSize)
-    })
-
-    it('is possible to catch errors thrown by iterable', async () => {
-      const cursorSize = 10
-      const asyncIterator = makeAsyncIterator(getCursorMock({ shouldThrow: true, cursorSize }))
-      let count = 0
-      try {
-        for await (const value of asyncIterator) {
-          assert.isOk(value)
-          count++
-        }
-        assert.fail('should throw')
-      } catch (err) {}
-      assert.equal(count, cursorSize)
-    })
-  })
-
   describe('rowCollector', () => {
     it('should yield an array containing specified number of elements or the final residual count', async () => {
       const cursorSize = 25
